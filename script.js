4280b2aa23e9c91c3503de8fcb2db3ead6bbe792
davideaded <david.castrodc033@gmail.com>
2025-03-18 16:04:12 -0300

    pages with iframe and styiling

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const mainNav = document.getElementById("main-nav");
+const pageTitle = document.getElementById("page-title");
+
+function callIframeDocument(docName) {
+  if (docName === "") return null;
+  const iframe = document.getElementsByTagName("iframe").item(0);
+  
+  console.log(docName)
+  iframe.src = `./pages/${docName}.html`;
+}
+
+const mainNavListItems = Array.from(
+  mainNav.children.item(0).children
+)
+
+mainNavListItems.forEach( li => {
+  li.addEventListener("click", () => {
+    let itemName = li.innerHTML.toLowerCase().trim(); 
+    callIframeDocument(itemName);
+  });
+});
